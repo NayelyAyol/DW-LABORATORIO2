e299808edc131129bf3bae57763a431143ee8226
Nayely Ayol <nayely.ayol@epn.edu.ec>
2025-10-12 17:06:57 -0500

    Culminacion de la seccion de arreglos

--- objetos-arreglos.js
+++ objetos-arreglos.js
@@ -65,3 +65,102 @@ const user = {
         "following": 100
     }
 }
+
+// ARREGLOS
+console.log("=============== ARREGLOS ===============");
+const publicaciones = [
+  {
+    titulo: "After a tough week",
+    fecha: "2025-10-06",
+    likes: 5300000,
+  },
+  {
+    titulo: "Up next. Send good vibes",
+    fecha: "2025-09-04",
+    likes: 1800000,
+  },
+  {
+    titulo: "Roscoe forever ",
+    fecha: "2025-09-29",
+    likes: 1700000,
+  }
+];
+
+console.log("------ PUBLICACIONES ------");
+console.log(publicaciones);
+
+// MÉTODO MAP
+const resumenPublicaciones = publicaciones.map((p) => {
+  return {
+    titulo: p.titulo,
+    popular: p.likes > 10000000 ? "Sí" : "No"
+  };
+});
+console.log("------ RESUMEN DE PUBLICACIONES ------");
+console.log(resumenPublicaciones);
+
+console.log("\n")
+
+
+// Arreglos
+const posts = [
+    {
+        id: 1,
+        link: "/alfonsoespinosadelosmonteros/reel/DPmu7F3knDW/",
+        description: "#tbt ¿Recuendan este programa? #QuienQuiereSerMillonario #donalfonso #alfonsoespinosadelosmonteros",
+        img: "https://scontent.cdninstagram.com/v/t51.71878-15/563441030_1188040883381293_5439498983644556853_n.jpg?stp=dst-jpg_e15_tt6&_nc_cat=103&ig_cache_key=MzczOTg4MjkxMTQ2NzQ2Njk2Ng%3D%3D.3-ccb1-7&ccb=1-7&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjY0MHgxMTM4LnNkci5DMyJ9&_nc_ohc=YVlcHskWg9gQ7kNvwF03Gm1&_nc_oc=AdlAhgPG_mtDu2DFIlU9Tgt1YEO-3eear0ylaZrA8m_HXnTPFOf2l59_ckcm4iilETY&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.cdninstagram.com&_nc_gid=tZ76iC_hGJG4HA7JIPGCQw&oh=00_AfcrtqkXB57AYmJx0OW_rWj3UY0-Q__qDl3uCYgwW2AA7w&oe=68F0FAC4"
+    },
+    {
+        id: 2,
+        link: "/alfonsoespinosadelosmonteros/reel/DPVM0VADL9O/",
+        description: "💪 #donalfonso #alfonsoespinosadelosmonteros #liderpleno",
+        img: "https://scontent.cdninstagram.com/v/t51.71878-15/559253039_1832377038155208_7478978812594119582_n.jpg?stp=dst-jpegr_e15_tt6&_nc_cat=110&ig_cache_key=MzczNDk0NzgzODQ5NDAzMTY5NA%3D%3D.3-ccb1-7&ccb=1-7&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjY0MHgxMTM2Lmhkci5DMyJ9&_nc_ohc=y9GrFkKx1O0Q7kNvwGgqwOS&_nc_oc=AdmQW7qX92-gyhSvY9eLJh0shs7Jz_4Qu05Kn_fRS-qZqJvC_9XAsinHted2tyKLE5o&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&se=-1&_nc_ht=scontent.cdninstagram.com&_nc_gid=tZ76iC_hGJG4HA7JIPGCQw&oh=00_Aff8rtBpEnpFlfiZIbBfbXdEAgcTyF3AhWqDGdsb6nOPSQ&oe=68F0FFFC"
+    },
+    {
+        id: 3,
+        link: "/alfonsoespinosadelosmonteros/reel/DPBtE11jlm_/",
+        description: "Un mensaje de paz y unidad",
+        img: "https://scontent.cdninstagram.com/v/t51.71878-15/553311922_1125312246244358_4759995586627669678_n.jpg?stp=dst-jpg_e15_tt6&_nc_cat=104&ig_cache_key=MzcyOTQ2MDIxMTIxNzIyNjE3NQ%3D%3D.3-ccb1-7&ccb=1-7&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjY0MHgxMTM4LnNkci5DMyJ9&_nc_ohc=8ITyzH935rcQ7kNvwFtD-vB&_nc_oc=Adl4Ky-k_83CoLCbHFIIxeJT1sVh8j0OTbbmWOdQlfQ9AoMCmpSF8kuqGhDBI3MssGc&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.cdninstagram.com&_nc_gid=tZ76iC_hGJG4HA7JIPGCQw&oh=00_AfeJp0YTpUKVyi2C-RLGrsnZWhbpiXy90hwrwzdRj_oiaw&oe=68F0FDFB"
+    }
+]
+
+const postsDescriptions = posts.map( post => post.description ); // Extrae solo las descripciones de los posts
+console.log("------ DESCRIPCIONES DE POSTS ------");
+postsDescriptions.forEach((desc, i) => {
+  console.log(`${i+1}. ${desc}`);
+});
+
+console.log("\n");
+
+//Arreglo de los tres primeros comentarios
+const comentarios = [
+{
+  usuario: "cristian.r4moss",
+  comentario: "El unico palito en toda la playa y te lo llevas por delante",
+  fecha: "2025-09-30",
+  likes: 8424
+},
+{
+  usuario: "cajlo.2",
+  comentario: "Por andar descalzo",
+  fecha: "2025-09-30",
+  likes: 3164
+}
+, 
+{
+  usuario: "andres_gigena10",
+  comentario: "No puso ni las manos",
+  fecha: "2025-09-30",
+  likes: 1147
+}                   
+]
+
+const usuarioComentario = comentarios.map(item => ({
+  usuario: item.usuario,
+  comentario: item.comentario
+}));
+
+console.log("------ COMENTARIOS DE USUARIOS ------");
+usuarioComentario.forEach((c, i) => {
+  console.log(`${i+1}. ${c.usuario}: "${c.comentario}"`);
+});
