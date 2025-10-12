c857e2d1b69b5ba8ce5a962ca0d4e60ae179e2d7
Nayely Ayol <nayely.ayol@epn.edu.ec>
2025-10-11 21:51:51 -0500

    Laboratorio 3 - DW

---
+++ fundamentos-funciones.js
@@ -0,0 +1,25 @@
+//------- LABORATORIO 3 - SPOTIFY --------//
+
+// VARIABLES
+var cantidadCancionesPopulares = 10;
+let usuarioPremium = false;
+const idArtistaSpotify = "0du5cEVh5yTK9QJze8zA0C";
+
+// STRINGS
+let nombreArtista = "Bruno Mars";
+let nacionalidadArtista = "Estadounidense";
+
+// NUMEROS ENTEROS
+let oyentesMensuales = 113489800
+let cantidadAlbumes = 5;
+
+// FLOAT
+let duracionCancionPopular = "3.1833";
+
+// BOOLEANOS
+let artistaVerificado = true;
+let siguiendoArtista = true;
+
+// ARRAYS
+let generoMusical = ["Pop", "R&B", "Funk"];
+let cincoCancionePopulares = ["Die With A Smile", "APT.", "Locked out of Heaven", "Just the Way You Are", "When I Was Your Man"];
